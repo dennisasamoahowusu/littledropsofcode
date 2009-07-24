152f044867fa69e61b583363a29fc6f7f90808ce
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-24 23:32:47 +0000

    Added registration page which currently only contains a registration form but needs to get a bit of text and visual touch-up so it doesn't look so horribly ugly.

---
+++ libldoc/pages/register.php
@@ -0,0 +1,18 @@
+<form method="post" action="index.php?action=reg">
+  Full name<br />
+  <input type="text" name="fullname" /><br />
+  
+  Username<br />
+  <input type="text" name="username" /><br />
+  
+  Password<br />
+  <input type="password" name="password" /><br />
+  
+  Confirm password<br />
+  <input type="password" name="password2" /><br />
+  
+  Email<br />
+  <input type="text" name="email"><br />
+
+  <input type="submit" name="Register" /><br />
+</form>
