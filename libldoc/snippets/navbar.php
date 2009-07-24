43c27583e03473578dacea0c4542e8515e065b7b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-24 21:44:28 +0000

    Added navbar. The items on the navbar are currently subject to change without prior notice.

---
+++ libldoc/snippets/navbar.php
@@ -0,0 +1,4 @@
+<ul id="navlist">
+  <li><a href="index.php?page=home">Home</a></li>
+  <li><a href="index.php?page=register">Register</a></li>
+</ul>
--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <?php
 define(LIBLDOC_PATH, '@LIBLDOC_PATH@');
-define(LIBLDOC_PAGES, LIBLDOC_PATH . '/pages/');
-define(LIBLDOC_SNIPPETS, LIBLDOC_PATH . '/snippets');
-define(LIBLDOC_ACTIONS, LIBLDOC_PATH . '/actions/');
-define(LIBLDOC_CLASSES, LIBLDOC_PATH . '/classes/');
+
+function snippet($snippet) { 
+    require(LIBLDOC_PATH . "/snippets/$snippet.php");
+}
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML 1.1">
@@ -16,6 +16,10 @@ define(LIBLDOC_CLASSES, LIBLDOC_PATH . '/classes/');
     <div id="page">
       <div id="page-header">
 	Litte drops of code
+      </div>
+
+      <div id="navbar">
+	<?php snippet("navbar"); ?>
       </div>
 
       <p>This is just a placeholder. To be removed.</p>
