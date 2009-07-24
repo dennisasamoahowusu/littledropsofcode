5303795271d82b95cd43f6ee3b129a5d76676d19
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-24 10:14:40 +0000

    Defined LIBLDOC_PATH to point to the path where libldoc was installed. This path is automatically set by ant.

--- www/index.php
+++ www/index.php
@@ -1,3 +1,11 @@
+<?php
+define(LIBLDOC_PATH, '@LIBLDOC_PATH@');
+define(LIBLDOC_PAGES, LIBLDOC_PATH . '/pages/');
+define(LIBLDOC_SNIPPETS, LIBLDOC_PATH . '/snippets');
+define(LIBLDOC_ACTIONS, LIBLDOC_PATH . '/actions/');
+define(LIBLDOC_CLASSES, LIBLDOC_PATH . '/classes/');
+?>
+
 <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML 1.1">
   <head>
     <title>Little drops of code</title>
