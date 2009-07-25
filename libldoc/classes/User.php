60051ce637fc29eb60e8e8efda5dfd7ca9e85ecf
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-25 10:41:08 +0000

    Created User class.

---
+++ libldoc/classes/User.php
@@ -0,0 +1,27 @@
+<?php
+class User {
+    private static $instance;
+    private $dbh;
+
+    private function __construct() {
+	$this->dbh = Database::instance()->connection();
+    }
+
+    public static function instance() {
+	if (!isset(self::$instance)) {
+	    $class = __CLASS__;
+	    self::$instance = new $class;
+	}
+
+	return self::$instance;
+    }
+
+    public function add($user) {
+	$query = "insert into users(username, fullname, password, email, "
+	    . "reg_date) values(?, ?, ?, ?, now())";
+	$sth = $this->dbh->prepare($query);
+	$sth->execute(array($user['username'], $user['fullname'],
+	    $user['password'], $user['email']));
+    }
+}
+?>
