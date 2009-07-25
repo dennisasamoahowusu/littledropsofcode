5222cce9f3959f814c1a411c23eda56d5591c366
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-25 10:06:41 +0000

    More style work on the registration form, including getting it centered and giving it a different background color.

--- libldoc/pages/register.php
+++ libldoc/pages/register.php
@@ -1,18 +1,20 @@
-<form method="post" action="index.php?action=register">
-  <span class="label">Full name</span><br />
-  <input type="text" name="fullname" /><br />
+<div class="form-container">
+  <form method="post" action="index.php?action=register">
+    <span class="label">Full name</span><br />
+    <input type="text" name="fullname" /><br />
   
-  <span class="label">Username</span><br />
-  <input type="text" name="username" /><br />
+    <span class="label">Username</span><br />
+    <input type="text" name="username" /><br />
   
-  <span class="label">Password</span><br />
-  <input type="password" name="password" /><br />
+    <span class="label">Password</span><br />
+    <input type="password" name="password" /><br />
   
-  <span class="label">Confirm password</span><br />
-  <input type="password" name="password2" /><br />
+    <span class="label">Confirm password</span><br />
+    <input type="password" name="password2" /><br />
   
-  <span class="label">Email</span><br />
-  <input type="text" name="email"><br />
+    <span class="label">Email</span><br />
+    <input type="text" name="email"><br />
 
-  <input type="submit" name="Register" /><br />
-</form>
+    <input type="submit" class="submit" name="Register" /><br />
+  </form>
+</div>
