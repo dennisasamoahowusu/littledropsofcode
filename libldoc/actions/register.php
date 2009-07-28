b8cf8710f53405afa79fdfebaf5043bec86284b8
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-28 23:33:01 +0000

    Perform secondary validation in register action.

--- libldoc/actions/register.php
+++ libldoc/actions/register.php
@@ -1,4 +1,11 @@
 <?php
-User::instance()->add($_POST);
-LittleDropsOfCode::instance()->setPage('home');
+$user = User::instance();
+$v = $user->validate($_POST);
+
+if ($v['valid']) {
+    User::instance()->add($_POST);
+    LittleDropsOfCode::instance()->setPage('home');
+} else {
+    LittleDropsOfCode::instance()->setPage('register');
+}
 ?>
