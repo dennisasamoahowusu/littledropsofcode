21cd72581851dc0ba891896233a684cb6ebf366b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-28 23:59:05 +0000

    Added introductory paragraph to registration page. This paragraph links to the "Privacy statement" and "Terms of service" pages, which I have created skeleton pages for. Somebody who knows how to write these things should step up and do it.

---
+++ libldoc/pages/privacy.php
@@ -0,0 +1,3 @@
+<h2>Privacy Statement</h2>
+
+<p><b>TODO:</b> write a privacy statement.</p>
--- libldoc/pages/register.php
+++ libldoc/pages/register.php
@@ -1,5 +1,9 @@
-<div class="form-container">
+<p>By registering on this site you become a part of our community and are able
+to upload code snippets. Before you register, you may want to take a look at
+our <a href="index.php?page=privacy">privacy statement</a>. You can also look
+at our <a href="index.php?page=terms">terms of service</a>.</p>
 
+<div class="form-container">
   <form method="post" action="index.php?action=register">
     <span class="label">Full name</span><br />
     <input type="text" name="fullname" /><br />
