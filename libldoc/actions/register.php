4e2a6f6ebbc53b2d07028598e7fd6b9737486655
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-29 01:55:53 +0000

    Created post-register page and linked it from the register action.

--- libldoc/actions/register.php
+++ libldoc/actions/register.php
@@ -4,7 +4,7 @@ $v = $user->validate($_POST);
 
 if ($v['valid']) {
     User::instance()->add($_POST);
-    LittleDropsOfCode::instance()->setPage('home');
+    LittleDropsOfCode::instance()->setPage('post-register');
 } else {
     LittleDropsOfCode::instance()->setPage('register');
 }
---
+++ libldoc/pages/post-register.php
@@ -0,0 +1,5 @@
+<p>Thank you for registering with us!</p>
+
+<p>You are now ready to joining our community. The first thing you will want
+to do is to <a href="index.php?page=login">log in</a>, so that you can begin
+to contribute.</p>
