bb5d1f6fb9ed52125071979750585d965affc359
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-31 01:22:50 +0000

    Created ActivityLog class.

---
+++ libldoc/classes/ActivityLog.php
@@ -0,0 +1,27 @@
+<?php
+class ActivityLog {
+    private static $instance;
+    private $dbh;
+
+    private function __construct() {
+	$this->dbh = Database::instance()->connection();
+    }
+
+    public static function instance() {
+	if (!isset(self::$instance)) {
+	    $class = __CLASS__;
+	    self::$instance = new $class;
+	}
+
+	return self::$instance;
+    }
+
+    public function log($log) {
+	$query = "insert into activity_log(log_date, log_type, log_user, "
+	    . "log_summary) values(now(), (select type_id from log_types "
+	    . "where log_type = 'registration'), ?, ?)";
+	$sth = $this->dbh->prepare($query);
+	$sth->execute(array($log['user'], $log['summary']));
+    }
+}
+?>
