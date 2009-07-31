06f8dff567a8cec9a42b93abc379110bbb1e2fc0
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-31 11:41:41 +0000

    Log user registration in the activity log.

--- libldoc/actions/register.php
+++ libldoc/actions/register.php
@@ -3,7 +3,11 @@ $user = User::instance();
 $v = $user->validate($_POST);
 
 if ($v['valid']) {
-    User::instance()->add($_POST);
+    $u = User::instance()->add($_POST);
+    $log = array('type' => 'Registration', 'user' => $u['user_id'],
+	'summary' => 'User ' . $u['username'] . ' registered');
+
+    ActivityLog::instance()->log($log);
     LittleDropsOfCode::instance()->setPage('post-register');
 } else {
     LittleDropsOfCode::instance()->setPage('register');
