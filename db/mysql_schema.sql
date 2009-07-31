9873a02f63ad07f11a58676b2f59642fba1eb1dd
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-31 00:55:49 +0000

    Added activity_log and log_types tables.

--- db/mysql_schema.sql
+++ db/mysql_schema.sql
@@ -10,3 +10,20 @@ create table users(
     primary key(user_id),
     unique(username),
     unique(email));                            
+
+drop table if exists log_types;
+create table log_types(
+    type_id int not null auto_increment,
+    log_type varchar(50),
+    primary key(type_id),
+    unique(log_type));
+
+drop table if exists activity_log;
+create table activity_log(
+    log_id int not null auto_increment,
+    log_date datetime not null,
+    log_type int not null,
+    log_user int not null,
+    log_summary text,
+    primary key(log_id));
+
