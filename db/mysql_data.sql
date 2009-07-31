06ac112ee094b8c93deeee653fd6b9727be9d45c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-31 01:28:34 +0000

    Insert registration log type in database.

--- db/mysql_data.sql
+++ db/mysql_data.sql
@@ -2,3 +2,4 @@ insert into users(user_id, username, password, fullname, email, reg_date)
     values(1, 'admin', 'nimda', 'System Administrator', 'postmaster@localhost',
     now());
 
+insert into log_types(log_type) values('Registration');
