52f1aa3ae3a62aa71b6bf7922b02b2cf291b9868
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-08-01 22:10:48 +0000

    Designed login form.

--- libldoc/pages/login.php
+++ libldoc/pages/login.php
@@ -1,1 +1,15 @@
-<p>Placeholder for login page</p>
+<p>If you have a user account, you can log in using the form below. If you do
+not yet have an account, you can <a href="index.php?page=register">register</a>
+for an account.</p>
+
+<div class="form-container">
+  <form method="post" action="index.php?action=auth">
+    <label for="username">Username</label><br />
+    <input type="text" name="username" id="username" /><br />
+  
+    <label for="password">Password</label><br />
+    <input type="password" name="password" id="password" /><br />
+
+    <input type="submit" class="submit" value="Log In" />
+  </form>
+</div>
