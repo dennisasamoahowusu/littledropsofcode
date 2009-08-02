e0204cd72b6e186519b9cb044317f543b5334b5b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-08-02 11:27:48 +0000

    Created handler for the authenticate action.

---
+++ libldoc/actions/auth.php
@@ -0,0 +1,12 @@
+<?php
+$auth = new Authenticator();
+$res = $auth->authenticate($_POST);
+
+if ($res['auth']) {
+    Session::instance()->login($_POST['username']);
+    LittleDropsOfCode::instance()->setPage('home');
+} else {
+    LittleDropsOfCode::instance()->setPage('login');
+    echo "MSG: " . $res['msg'] . '<br />';
+}
+?>
