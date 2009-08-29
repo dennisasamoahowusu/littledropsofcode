e17868ab412d4c816e7813112ad8049dcc8d9697
Dennis <dennisasamoahowusu@yahoo.com>
2009-08-29 06:17:30 -0700

    added some content to profile.php

--- libldoc/pages/profile.php
+++ libldoc/pages/profile.php
@@ -1,1 +1,22 @@
+<h3>Profile of </h3>
+
+<div id="photograph">
+  <p class="left-column">Photograph</p>
+  <p class="right-column">User's photograph</p>
+</div>
+
+<div id="name">
+  <p class="left-column">Full Name</p>
+  <p class="right-column">User's full name</p>
+</div>
+
+<div id="gender">
+  <p class="left-column">Gender</p>
+  <p class="right-column">User's gender</p>
+</div>
+
+<div id="programming-language">
+  <p class="left-column">Programming Language</p>
+  <p class="right-column">User's programming Language</p>
+</div>
 
